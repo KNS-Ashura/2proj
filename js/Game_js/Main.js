255aec8d564a4d52c0a558743b04936d11697778
KNS-Ashura <160181900+KNS-Ashura@users.noreply.github.com>
2026-01-15 10:02:06 +0100

    export and import in js

---
+++ js/Game_js/Main.js
@@ -0,0 +1,15 @@
+import Phaser from "phaser";
+import MainScene from "./Map/Map.js";
+
+const config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scene: MainScene,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+};
+
+new Phaser.Game(config);
